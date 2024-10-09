c525a284835f41d513ff4ba8d61f4b1f31d40f66
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 03:09:55 +0530

    Update shared.py

--- shared.py
+++ shared.py
@@ -2,13 +2,27 @@
 API = "your_api"
 URL = "your_url"
 
-def check_token():
-    # Implementation of check_token
-    pass
+async def check_token(bot, userid, token):
+    user = await bot.get_users(userid)
+    if user.id in TOKENS.keys():
+        TKN = TOKENS[user.id]
+        if token in TKN.keys():
+            is_used = TKN[token]
+            if is_used == True:
+                return False
+            else:
+                return True
+    else:
+        return False
 
-def get_token():
-    # Implementation of get_token
-    pass
+async def get_token(bot, userid, link):
+    user = await bot.get_users(userid)
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    TOKENS[user.id] = {token: False}
+    link = f"{link}verify-{user.id}-{token}"
+    shortened_verify_url = await get_verify_shorted_link(link)
+    return str(shortened_verify_url)
+
 async def verify_user(bot, userid, token):
     user = await bot.get_users(userid)
     TOKENS[user.id] = {token: True}
