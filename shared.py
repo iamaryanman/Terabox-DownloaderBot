2052acbb20d98dfcc27d867d3b3fe224b43fff53
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 03:02:13 +0530

    Update shared.py

--- shared.py
+++ shared.py
@@ -6,6 +6,18 @@ async def verify_user(bot, userid, token):
     today = date.today()
     VERIFIED[user.id] = str(today)
 
-def check_verification():
-    # Implementation
+async def check_verification(bot, userid):
+    user = await bot.get_users(userid)
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    if user.id in VERIFIED.keys():
+        EXP = VERIFIED[user.id]
+        years, month, day = EXP.split('-')
+        comp = date(int(years), int(month), int(day))
+        if comp<today:
+            return False
+        else:
+            return True
+    else:
+        return False
   
