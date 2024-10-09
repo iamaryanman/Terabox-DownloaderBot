2f9de572b1044cb3f689faa8827a522d8ec1d561
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 03:14:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from terabox import API, URL
 from shortzy import Shortzy
 
 # utils.py
-from shared import check_verification
+from shared import check_verification, check_token, get_token
 
 TOKENS = {}
 VERIFIED = {}
