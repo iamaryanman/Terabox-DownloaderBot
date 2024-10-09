268b222a0709a7d6e59b9ffd7f261850acaa402c
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 03:01:04 +0530

    Update shared.py

--- shared.py
+++ shared.py
@@ -1,6 +1,10 @@
 # shared.py
-def verify_user():
-    # Implementation
+async def verify_user(bot, userid, token):
+    user = await bot.get_users(userid)
+    TOKENS[user.id] = {token: True}
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    VERIFIED[user.id] = str(today)
 
 def check_verification():
     # Implementation
