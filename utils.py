cf6dfea1f4bc063454bc3f34dfe02196ff412eed
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 03:09:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,25 +14,3 @@ async def get_verify_shorted_link(link):
     link = await shortzy.convert(link)
     return link
 
-async def check_token(bot, userid, token):
-    user = await bot.get_users(userid)
-    if user.id in TOKENS.keys():
-        TKN = TOKENS[user.id]
-        if token in TKN.keys():
-            is_used = TKN[token]
-            if is_used == True:
-                return False
-            else:
-                return True
-    else:
-        return False
-
-async def get_token(bot, userid, link):
-    user = await bot.get_users(userid)
-    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
-    TOKENS[user.id] = {token: False}
-    link = f"{link}verify-{user.id}-{token}"
-    shortened_verify_url = await get_verify_shorted_link(link)
-    return str(shortened_verify_url)
-
-
