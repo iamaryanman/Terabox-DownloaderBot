dbaa01ba953d34cf543336e01e25051b48a24b41
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 10:30:58 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,1 @@
-# constants.py
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/botsbyadmin/5") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "downloadteraboxvideobot") # bot username without @
-VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
+
