14e2ea3d8413406900d09277809262871091e103
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 10:11:00 +0000

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -18,11 +18,6 @@ from web import keep_alive
 from shared import API, URL, check_token, get_token, verify_user, check_verification, get_verify_shorted_link
 from constants import VERIFY, VERIFY_TUTORIAL, BOT_USERNAME
 
-API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
-VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 
 load_dotenv('config.env', override=True)
 
