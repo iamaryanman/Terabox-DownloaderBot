ec413949f935bf80b8341dffb576c010b9b68d2c
iamaryanman <aryan5566.ji@gmail.com>
2024-10-10 10:31:57 +0000

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -16,7 +16,11 @@ from web import keep_alive
 
 # terabox.py
 from shared import API, URL, check_token, get_token, verify_user, check_verification
-from constants import VERIFY, VERIFY_TUTORIAL, BOT_USERNAME
+
+# constants.py
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/botsbyadmin/5") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "downloadteraboxvideobot") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 
 
 load_dotenv('config.env', override=True)
